b35b868599d7c5f1a5008a727ea0cefd1aa88428
Sharmin Moon <sharminmoon0308@gmail.com>
2022-12-08 01:05:32 +0600

    resume link updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/sharminmoon/portfolio/master/src/Assets/resume-of-sharmin.pdf";
+  "https://raw.githubusercontent.com/sharminmoon/portfolio/main/src/Assets/resume-of-sharmin.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
