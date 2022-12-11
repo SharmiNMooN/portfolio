fecd06c7941a2e57a96eb10f935ecdc24b6851df
Sharmin Moon <sharminmoon0308@gmail.com>
2022-12-11 17:59:58 +0600

    copyright name updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
           <h3>Designed and Developed by Sharmin Akter</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} sharminmoon</h3>
+          <h3>Copyright © {year} Sharmin Akter</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
